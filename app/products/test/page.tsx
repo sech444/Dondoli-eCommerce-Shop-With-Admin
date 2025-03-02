c8b5d2d01ca1ebadf96b43194ef04dad6dfbd200
sech444 <sech444@gmail.com>
2025-03-02 16:08:22 +0100

    using pages

---
+++ app/products/test/page.tsx
@@ -0,0 +1,8 @@
+// app/products/test/page.tsx
+export default function TestPage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Test Page Works!</h1>
+      </div>
+    );
+  }
