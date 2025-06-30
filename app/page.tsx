a63fc9d12c76fb8df7ca1e7e1dce4603e65521b6
sech444 <sech444@email.com>
2025-06-30 03:19:09 -0400

    Update landing page and main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-
-
-// app/page.tsx
 "use client";
 
 import { CategoryMenu, Hero, IntroducingSection, ProductsSection } from "@/components";
@@ -12,17 +9,16 @@ export default function Home() {
 
   return (
     <main className="min-h-screen relative">
-    {/* Background div with specific size, positioned absolutely */}
-    <div 
-      className="bg-no-repeat bg-bottom ...[url('/background-2.jpg')] bg-contain bg-no-repeat"
-      // w-96 = 384px, h-96 = 384px
-    />
-     <IntroducingSection open={showLanding} onClose={() => setShowLanding(false)} />
-    
-    {/* Content container */}
-    <div className="relative min-h-screen">
+      {/* Background div with specific size, positioned absolutely */}
+      <div 
+        className="bg-no-repeat bg-bottom bg-[url('/background-2.jpg')] bg-contain"
+        // w-96 = 384px, h-96 = 384px
+      />
+      <IntroducingSection open={showLanding} onClose={() => setShowLanding(false)} />
+      
+      {/* Content container */}
+      <div className="relative min-h-screen">
         <Hero />
-        {/* <IntroducingSection /> */}
         {/* <CategoryMenu /> */}
         <ProductsSection />
         <Testimonials />
